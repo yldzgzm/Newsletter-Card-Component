dafa8468d5029b066d4d6df73f78cde7361aa165
Gizem Özyıldız <74597715+yldzgzm@users.noreply.github.com>
2024-04-06 23:50:20 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="newsletter-Card">
+                <text className="header">Level Up Your Frontend Skills</text>
+                <text className="text">Sign up for our free newsletter to receive weekly coding challenges that
+                    will help you improve your frontend development skills.
+                </text>
+                <div className="email-container">
+                    <div>
+                        <input type="email" placeholder="Enter your email address" className="input-box" />
+                    </div>
+                    <button className="button" style={{ color: 'white' }} onClick={() => alert("Thank you for subscribe!")}>SUBSCRIBE</button>
+                </div>
+                <div className="checkbox">
+                    <input type="checkbox" id="box" />
+                    <label htmlFor="checkbox" style={{ fontSize:'2vh' }}>
+                        By checking this box, you agree to receive our weekly newsletter containing coding challenges, tips, and other related content. You may unsubscribe from the newsletter at any time.
+                    </label>
+                </div>
+
+                {/*final*/}
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
